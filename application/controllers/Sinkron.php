777f3b24bcf07344cd2a0b897fca428f0ef4ff08
buttonsoft <buttonsoft1@gmail.com>
2019-09-07 10:46:39 +0800

    Menambahkan fitur chained dropdown

--- application/controllers/Sinkron.php
+++ application/controllers/Sinkron.php
@@ -6,19 +6,19 @@ class Sinkron extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        $this->load->helper(array('url', 'form'));
-        $this->load->database();
         $this->load->model('dropdown_m');
     }
     function index()
     {
-        $data['kelas'] = $this->dropdown_m->nm_kelas();
+        $kelas = $this->dropdown_m;
+        $data['kelas'] = $kelas->nm_kelas();
         $this->load->view('sinkron/depan', $data);
     }
     function siswa()
     {
+        $siswa = $this->dropdown_m;
         $id = $this->input->post('id_kelas');
-        $data['siswa'] = $this->dropdown_m->nm_siswa($id);
+        $data['siswa'] = $siswa->nm_siswa($id);
         $this->load->view('sinkron/siswa', $data);
     }
 }
